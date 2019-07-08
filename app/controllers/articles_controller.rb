49ac93e609e88fb9cf9ab3ce9ed78f732a75cb4b
Yangtao Hu <yangt.1994@gmail.com>
2019-07-08 00:35:29 -0400

    Added in flash messages when creating posts, and redirecting to the correct show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,17 +7,30 @@ class ArticlesController < ApplicationController
   def create
     #show the information
     #render plain: params[:article].inspect 
-    #save
+    #save only if validations are met, else display an error message
     @article = Article.new(article_params)
-    @article save 
-    #redirect to show page articles#show
-    redirect_to articles_show(@article)
-
+    if @article.save
+      flash[:notice] = "Article was successfully created!"
+      redirect_to article_path(@article)
+    else
+        #call new again
+        render 'new'
+        
+    end
+    #redirect to show page articles#path
+    
   end
 
+
+  def show
+    #find the article based on the ID
+    #create a template to show the article
+    @article = Article.find(params[:id])
+  end
   private
     def article_params
       # get the title and description
       params.require(:article).permit(:title, :description)
     end
+  
 end
