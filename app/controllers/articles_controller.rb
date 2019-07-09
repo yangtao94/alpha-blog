b161a5cdf6c87b83fe13710f4b285dc3e862827f
Yangtao Hu <yangt.1994@gmail.com>
2019-07-09 17:35:33 -0400

    Completed authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
     #render plain: params[:article].inspect 
     #save only if validations are met, else display an error message
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created!"
       redirect_to article_path(@article)
